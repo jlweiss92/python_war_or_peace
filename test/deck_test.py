a35e60c53b3bf005303184687407398b7e64ae18
Austin Aspaas <araspaas@live.com>
2022-01-07 16:32:34 -0700

    Add test for rank_of_card_at method

--- test/deck_test.py
+++ test/deck_test.py
@@ -11,3 +11,18 @@ def test_it_exists():
     cards = [card1, card2, card3]
     deck = Deck(cards)
     assert len(deck.cards) == 3
+
+def test_rank_of_card_at():
+    card1 = Card("heart", "King", 13)
+    card2 = Card("spade", "5", 5)
+    card3 = Card("club", "8", 8)
+    card4 = Card("diamond", "Queen", 12)
+    cards = [card1, card2, card3]
+    empty = []
+    deck = Deck(cards)
+    empty_deck = Deck(empty)
+
+    assert deck.rank_of_card_at(0) == 13
+    assert deck.rank_of_card_at(1) == 5
+    assert deck.rank_of_card_at(2) == 8
+    assert empty_deck.rank_of_card_at(0) == 0
