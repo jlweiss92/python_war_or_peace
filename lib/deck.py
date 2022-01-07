dc2ad8775d349fd85c9416056f1d4123c560d618
Austin Aspaas <araspaas@live.com>
2022-01-07 16:34:03 -0700

    Add method rank_of_card_at. tests pass

--- lib/deck.py
+++ lib/deck.py
@@ -2,3 +2,9 @@ class Deck:
 
     def __init__(self, cards):
         self.cards = cards
+
+    def rank_of_card_at(self, index):
+        if self.cards == []:
+            return 0
+        else:
+            return self.cards[index].rank
