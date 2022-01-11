41d42ad1ad79caa53527764ca20394eb644e7e97
Austin Aspaas <araspaas@live.com>
2022-01-10 18:13:31 -0700

    Add high ranking cards method. Test passes

--- lib/deck.py
+++ lib/deck.py
@@ -1,14 +1,32 @@
 class Deck:
 
-    def __init__(self, cards): #creating a deck of cards
+    def __init__(self, cards): #creating a deck of cards.
         self.cards = cards
 
-    def rank_of_card_at(self, index): #checking the rank of a card in the deck
+    def rank_of_card_at(self, index): #checking the rank of a card in the deck.
         if self.cards == []:
             return 0
         else:
             return self.cards[index].rank
 
+    def high_ranking_cards(self): # Checking for cards that rank above 10.
+        high = []
+        for card in self.cards: #@cards.each do |card|
+            if card.rank > 10: # The if statement is the same
+                high.append(card) # This is the shovel method if the condition is met
+        return high # This is the return value we are looking for
+
+    #below is how I solved this in Ruby as a reference
+
+    #def high_ranking_cards
+        #high_ranked_cards = []
+        #@cards.each do |card|
+            #if card.rank > 10
+            #high_ranked_cards << card
+            #end
+        #end
+        #high_ranked_cards
+    #end
 
 
 
